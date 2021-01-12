602d5b6c8a69612627899463bee8bd0036749e48
aleksandrkondratev <ottacke1991@gmail.com>
2021-01-12 22:29:32 +0400

    5 min

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const token = '1519168195:AAGFOXzuDGKQmRoBn-KfZjOLLGDTHJKBe8c';
 const Telegraf = require('telegraf')
 const mongoose = require('mongoose')
 const psSchema = require('./schemas/psSchema')
+
 require("dotenv").config()
 
 const mongoURI = process.env.MONGOURI
@@ -26,12 +27,18 @@ bot.hears('проверь ps5', async (ctx) => {
     console.log('can talk')
 
     try {
+        const ozon = process.env.ozon
         const objectFromDb = await psSchema.findOne().lean()
         await ctx.replyWithMarkdown(
             `*${objectFromDb.shopStats.eldoDisc.shop} ${objectFromDb.shopStats.eldoDisc.city}*\n`+"=================="+`\n*Sony PlayStation 5 Blu-ray disc*\nДисковод: ${objectFromDb.shopStats.eldoDisc.psVersion ? 'Есть' : 'Нет'}\n`
             +`В наличии: ${objectFromDb.shopStats.eldoDisc.available ? 'Да' : 'Нет'}\nДата последней проверки:  ${objectFromDb.shopStats.eldoDisc.curDate}\n`+
             "=================="+`\n*Sony PlayStation 5 Digital Edition*\nДисковод: ${objectFromDb.shopStats.eldoDE.psVersion ? 'Есть' : 'Нет'}\n`
-            +`В наличии: ${objectFromDb.shopStats.eldoDE.available ? 'Да' : 'Нет'}\nДата последней проверки:  ${objectFromDb.shopStats.eldoDE.curDate}`
+            +`В наличии: ${objectFromDb.shopStats.eldoDE.available ? 'Да' : 'Нет'}\nДата последней проверки:  ${objectFromDb.shopStats.eldoDE.curDate}\n`+
+            "==================\n"+
+            `Ozon: ${objectFromDb.shopStats.Ozon.foundStatus ? 'Нашли консоль ' : 'Консоль не найдена'}\n`+
+            `Ссылка на поиск Ozon [Ozon.ru](https://www.ozon.ru/category/igry-i-soft-13300/?from_global=true&text=playstation+5)\n`+
+            `Количество вариантов консоли на Ozon ${objectFromDb.shopStats.Ozon.foundStatus ? objectFromDb.shopStats.Ozon.found.length : '0' }\n`+
+            `Дата последней проверки: ${objectFromDb.shopStats.Ozon.curDate}\n`
 
 
         )
